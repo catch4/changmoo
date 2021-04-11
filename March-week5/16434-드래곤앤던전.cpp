38f4902b7300b548eface1bff042a1d6556ee410
changmoomoon <public.stevemoon@gmail.com>
2021-04-11 22:49:26 +0900

    solved 16434

--- March-week5/16434-드래곤앤던전.cpp
+++ March-week5/16434-드래곤앤던전.cpp
@@ -1,23 +1,38 @@
+#include <algorithm>
 #include <climits>
 #include <iostream>
 #include <vector>
 using namespace std;
+#define ull unsigned long long
 
 struct Room {
-    int t, a, h;
+    int t, a, h;  //
+    // t가 1이면 몬스터 공격력 a, 생명력 h 1,000,000
+    // t가 2이면 용사 공격력 a증가, 생명력 h증가
 };
 
-struct Man {
-    long long maxHP, currHP, atk;
-    Man(int _x, int _y, int _z) : maxHP(_x), currHP(_y), atk(_z) {}
-};
+int n, atk;  // 방갯수123456,초기공격력1,000,000
+vector<Room> room;
 
-int n, atk;  // 방개수123456, 초기공격력1000000
-vector<Room> v;
+bool solve(ull atk, ull maxHP) {
+    ull currHP = maxHP;
 
-bool solve(int x) {
-    Man man = Man(x, x, atk);
-    vector<Room> v2 = v;
+    for (int i = 0; i < n; ++i) {
+        if (room[i].t == 1) {           // 몬스터방인 경우
+            ull cnt = room[i].h / atk;  // 공격횟수, 용사 선공
+
+            if (room[i].h % atk > 0) cnt++;         // 막타
+            if (currHP <= (cnt - 1) * room[i].a) {  // 트라이 실패
+                return false;
+            }
+            currHP -= (cnt - 1) * room[i].a;          // 공격횟수 * 몬스터 공격력
+        } else {                                      // 물약방인 경우
+            currHP = min(currHP + room[i].h, maxHP);  // 물약 먹어도 maxHP 이상 못 채움
+            atk += room[i].a;
+        }
+    }
+
+    return true;
 }
 
 int main() {
@@ -26,23 +41,19 @@ int main() {
     cout.tie(nullptr);
 
     cin >> n >> atk;
+    room.resize(n);
     for (int i = 0; i < n; ++i) {
-        int x, y, z;
-        cin >> x >> y >> z;
-        v.push_back({x, y, z});
+        cin >> room[i].t >> room[i].a >> room[i].h;
     }
 
-    long long start = 0;
-    long long end = LLONG_MAX;
-    long long ans = -1;
-
-    while (start <= end) {
-        int mid = (start + end) / 2;
-        if (solve(mid)) {
-            ans = mid;
-            end = mid - 1;
+    ull l = 0, r = ULLONG_MAX, ans = 0;
+    while (l <= r) {
+        ull maxHP = (l + r) / 2;
+        if (solve(atk, maxHP)) {
+            r = maxHP - 1;
+            ans = maxHP;
         } else {
-            start = mid + 1;
+            l = maxHP + 1;
         }
     }
 
