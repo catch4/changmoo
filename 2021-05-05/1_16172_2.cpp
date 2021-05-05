de4e6bd0f7351b3c5a82b8fb3c1d1c52dee8b786
changmoo moon <public.stevemoon@gmail.com>
2021-05-05 19:33:26 +0900

    fixed 16172

--- 2021-05-05/1_16172_2.cpp
+++ 2021-05-05/1_16172_2.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-int fail[200001];  // prefix suffix 일치하는 부분문자열 길이
-string s, s2, k;   // s2에서 패턴k를 검색한다 kmp
+int pi[200001];   // prefix suffix 일치하는 부분문자열 길이
+string s, s2, k;  // s2에서 패턴k를 검색한다 kmp
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -16,12 +16,17 @@ int main() {
         if (!isdigit(ch)) s2.push_back(ch);
 
     for (int i = 1, j = 0; i < k.length(); ++i) {
-        while (j && k[i] != k[j]) j = fail[j - 1];
-        if (k[i] == k[j]) fail[i] = ++j;
-    }
+        while (j > 0 && k[i] != k[j]) {
+            j = pi[j - 1];
+        }
+        if (k[i] == k[j]) {
+            pi[i] = j + 1;
+            j++;
+        }
+    }  // s2의 index 마다 prefix == suffix인 최대 부분문자열 길이를 구한다.
 
     for (int i = 0, j = 0; i < s2.length(); ++i) {
-        while (j && s2[i] != k[j]) j = fail[j - 1];
+        while (j > 0 & s2[i] != k[j]) j = pi[j - 1];  // 같을 때까지 비교
         if (s2[i] == k[j]) {
             if (j == k.length() - 1) {
                 cout << 1;
